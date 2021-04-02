afc3236236e313aa40d071ed5febc9927d7d105a
Robert Freeman <robfreeman01@hotmail.com>
2021-04-02 20:20:05 +0100

    Remove unused imports

--- app/src/main/java/com/freeman/hubfinder/model/RemoteRepository.kt
+++ app/src/main/java/com/freeman/hubfinder/model/RemoteRepository.kt
@@ -1,9 +1,6 @@
 package com.freeman.hubfinder.model
 
 import io.reactivex.rxjava3.core.Single
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Inject
 
 class RemoteRepository @Inject constructor(
