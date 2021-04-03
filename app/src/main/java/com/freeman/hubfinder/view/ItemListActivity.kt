7c7e9d295ec28996f2ea5fe06c972f184adaccbe
Robert Freeman <robfreeman01@hotmail.com>
2021-04-03 14:57:18 +0100

    Add collapsable toolbar

--- app/src/main/java/com/freeman/hubfinder/view/ItemListActivity.kt
+++ app/src/main/java/com/freeman/hubfinder/view/ItemListActivity.kt
@@ -1,6 +1,7 @@
 package com.freeman.hubfinder.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.core.widget.NestedScrollView
 import androidx.appcompat.app.AppCompatActivity
